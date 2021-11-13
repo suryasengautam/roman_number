4fd3db7efcae8db5ffe32a1c3e6badba41b9bbc4
dhruv <dhruv20@navgurukul.org>
2021-11-13 15:33:51 +0530

    suyasen

---
+++ roman_number.py
@@ -0,0 +1,16 @@
+# #roman number 1 too 3000
+# number= [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+# roman = { 1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL',
+#               50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'}
+# given_number = int(input("Enter a number: "))
+# if given_number<=3000:
+#     for i in number:
+#         if given_number != 0:
+#             quotient= given_number//i
+#             given_number = given_number%i
+#             if quotient != 0:
+#                 for y in range(quotient):
+#                     print(roman[i], end="")
+# else:
+#     print("enter less than 3001")
+
